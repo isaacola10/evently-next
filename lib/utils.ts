129e3a33ef81299b96d61c541cb89148ff661d5e
Isaac <isaacolaolu10@gmail.com>
2023-12-22 17:04:06 +0000

    New push with new fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -32,11 +32,11 @@ export const formatDateTime = (dateString: Date | undefined) => {
     hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
   };
 
-  const formattedDateTime: string = new Date(dateString).toLocaleString("en-US", dateTimeOptions);
+  const formattedDateTime: string = new Date(dateString!).toLocaleString("en-US", dateTimeOptions);
 
-  const formattedDate: string = new Date(dateString).toLocaleString("en-US", dateOptions);
+  const formattedDate: string = new Date(dateString!).toLocaleString("en-US", dateOptions);
 
-  const formattedTime: string = new Date(dateString).toLocaleString("en-US", timeOptions);
+  const formattedTime: string = new Date(dateString!).toLocaleString("en-US", timeOptions);
 
   return {
     dateTime: formattedDateTime,
